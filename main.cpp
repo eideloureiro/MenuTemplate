65be988d5f67b9f171596f30c73e693f86c25b07
Mileide Loureiro <37318876+eideloureiro@users.noreply.github.com>
2019-05-05 23:49:58 -0300

    P1

--- main.cpp
+++ main.cpp
@@ -22,18 +22,26 @@ int main() {
     // Example menu for a game:
     MenuTemplate::MenuTemplate MenuA;
 
-    MenuA.Title = "\t\tMenuTemplate 1.04";
-    MenuA.Description = "Choose with UP and DOWN, select with RETURN!";
-    MenuA.setCursor("> ");
-
-    MenuA.addEntry("GAME_NEW", "New game");
+	MenuA.Title = "\t\tNeverwinter Nights Simple Menu";
+    MenuA.Description = "Escolha com UP e DOWN, selecione com RETURN!";
+    MenuA.setCursor ("> ");
+    MenuA.setCursorLength(MenuA.getCursor().length());
+    
+    
+	MenuA.addEntry("GAME_NEW", "New game");
     MenuA.addEntry("GAME_LOAD", "Load save state");
     MenuA.addEntry("GAME_EXIT", "Exit game");
+    MenuA.addEntry("VIDEO_OPTIONS", "Video Options");
+    MenuA.addEntry("SOUND_OPTIONS", "Sound Options");
+    MenuA.addEntry("CONTROLS", "Controls");
     MenuA.addEntry("GAME_OPTIONS", "Game options");
-
+    
     MenuA.swapEntries(2, "GAME_OPTIONS");
 
-    MenuA.setCursorStartPosition(2);
+    MenuA.setCursorStartPosition(0);
 
     cout << MenuA.displayGetName() << endl;
+    cout << "Exibindo a funçao getCursor, com o cursor: " << MenuA.getCursor() << endl;
+    cout << "Tamanho do cursor: " << MenuA.getCursorLength() << endl;
+    system("pause");
 }
